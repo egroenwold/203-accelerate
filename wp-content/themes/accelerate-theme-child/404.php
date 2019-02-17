b854d12ce0428567cbce2a6ae7fbfc080a5a937e
egroenwold <evelyngcampos@gmail.com>
2019-02-17 18:13:30 +0100

    404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for 404 error page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<div class="content-wrapper-404">
+				<figure>
+					<img src="img/error-page.png" />
+				</figure>
+				<h2>Whoops!</h2>
+				<p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleading you. </p>
+				<p>Feel free to take a look around our <a href="<?php echo site_url('blog') ?>">blog</a> or some of our featured <a href="<?php echo site_url('archive/case-studies') ?>">work</a>. </p>
+
+			</div>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
